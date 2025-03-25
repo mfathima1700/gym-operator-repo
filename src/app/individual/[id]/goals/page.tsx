8897ead6ae1d66ca14ac358ec92d219d3b8c6f62
mfathima1700 <64264231+mfathima1700@users.noreply.github.com>
2025-03-25 10:41:13 +0000

    made new goals list look better

--- src/app/individual/[id]/goals/page.tsx
+++ src/app/individual/[id]/goals/page.tsx
@@ -2,42 +2,58 @@
 
 import CNLayout from "@/components/layout/cn-layout";
 import OldGoalsList from "@/components/goals/OldGoalsList";
-import NewGoalsList from "@/components/goals/NewGoalsList"
+import NewGoalsList from "@/components/goals/NewGoalsList";
+import { useEffect, useRef, useState } from "react";
+import { getUserById } from "@/redux/actions/GymActions";
+import { AppDispatch, RootState } from "@/redux/store";
+import { useParams, useRouter } from "next/navigation";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Goals() {
+  const dispatch = useDispatch<AppDispatch>();
+  const router = useRouter();
+  const params = useParams();
+  const id = Array.isArray(params.id) ? params.id[0] : (params.id ?? "");
+  const triggerRef = useRef<HTMLButtonElement>(null);
+  const updateOwnerSettingsState = useSelector(
+    (state: RootState) => state.updateOwnerSettings
+  );
+  const userState = useSelector((state: RootState) => state.getUser);
+  const [userData, setUserData] = useState(() => ({
+    gym: {
+      id: "",
+      classes: [],
+    },
+  }));
+
+  useEffect(() => {
+    dispatch(getUserById(id));
+  }, [id]);
+
+  useEffect(() => {
+    console.log(userState.user);
+    if (userState.user) {
+      setUserData(userState.user);
+    }
+  }, [userState.user, userState.success, userState.error]);
+
   return (
     <>
-      <CNLayout>
-      <div className="flex h-full">
+      <CNLayout user={userData} id={id}>
+        <div className="flex flex-col md:flex-row w-full gap-4">
+          {/* Left Column */}
+          <div className="w-full md:w-1/2  p-4">
+            <h2 className="text-xl font-bold mb-4">Goals in Progress</h2>
+            <NewGoalsList />
+          </div>
 
-        
+          <div className="w-full md:w-1/2  p-4">
+            <h2 className="text-xl font-bold mb-4">Completed Goals</h2>
 
-{/* Content area */}
-<div className="flex flex-1 flex-col overflow-hidden">
-  
-
-  {/* Main content */}
-  <div className="flex flex-1 items-stretch overflow-hidden">
-    <main className="flex-1 overflow-y-auto">
-      {/* Primary column */}
-      <section aria-labelledby="primary-heading" className="flex h-full min-w-0 flex-1 flex-col lg:order-last px-4">
-        <h1 id="primary-heading" className="sr-only">
-          Photos
-        </h1>
-        {/* Your content */}
-        <NewGoalsList/>
-      </section>
-    </main>
-
-    {/* Secondary column (hidden on smaller screens) */}
-    <aside className="hidden w-96 overflow-y-auto  lg:block px-4">
-      {/* Your content */}
-
-      <OldGoalsList/>
-    </aside>
-  </div>
-</div>
-</div>
+            <OldGoalsList />
+          </div>
+        </div>
       </CNLayout>
-      </>
-  )}
+    </>
+  );
+}
--- src/components/goals/NewGoalsList.tsx
+++ src/components/goals/NewGoalsList.tsx
@@ -1,75 +1,84 @@
-import { Checkbox } from "@/components/ui/checkbox"
+import { Checkbox } from "@/components/ui/checkbox";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Label } from "../ui/label";
+import { Textarea } from "../ui/textarea";
 
 type Comment = {
-    id: number;
-    name: string;
-    imageUrl: string;
-    content: string;
-    date: string;
-    dateTime: string;
-  };
-  
-  const comments: Comment[] = [
-    {
-      id: 1,
-      name: 'Leslie Alexander',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      content:
-        'Explicabo nihil laborum. Saepe facilis consequuntur in eaque. Consequatur perspiciatis quam. Sed est illo quia. Culpa vitae placeat vitae. Repudiandae sunt exercitationem nihil nisi facilis placeat minima eveniet.',
-      date: '1d ago',
-      dateTime: '2023-03-04T15:54Z',
-    },
-    {
-      id: 2,
-      name: 'Michael Foster',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      content:
-        'Laudantium quidem non et saepe vel sequi accusamus consequatur et. Saepe inventore veniam incidunt cumque et laborum nemo blanditiis rerum. A unde et molestiae autem ad. Architecto dolor ex accusantium maxime cumque laudantium itaque aut perferendis.',
-      date: '2d ago',
-      dateTime: '2023-03-03T14:02Z',
-    },
-    {
-      id: 3,
-      name: 'Dries Vincent',
-      imageUrl:
-        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      content:
-        'Quia animi harum in quis quidem sint. Ipsum dolorem molestias veritatis quis eveniet commodi assumenda temporibus. Dicta ut modi alias nisi. Veniam quia velit et ut. Id quas ducimus reprehenderit veniam fugit amet fugiat ipsum eius. Voluptas nobis earum in in vel corporis nisi.',
-      date: '2d ago',
-      dateTime: '2023-03-03T13:23Z',
-    },
-    {
-      id: 4,
-      name: 'Lindsay Walton',
-      imageUrl:
-        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      content:
-        'Unde dolore exercitationem nobis reprehenderit rerum corporis accusamus. Nemo suscipit temporibus quidem dolorum. Nobis optio quae atque blanditiis aspernatur doloribus sit accusamus. Sunt reiciendis ut corrupti ab debitis dolorem dolorem nam sit. Ducimus nisi qui earum aliquam. Est nam doloribus culpa illum.',
-      date: '3d ago',
-      dateTime: '2023-03-02T21:13Z',
-    },
-  ];
-  
-  export default function NewGoalsList(): JSX.Element {
-    return (
-      <ul role="list" className="divide-y divide-gray-800">
-        {comments.map((comment) => (
-          <li key={comment.id} className="flex gap-x-4 py-5">
-            
-            <div className="flex-auto">
-              <div className="flex items-baseline justify-between gap-x-4">
-                <p className="text-sm/6 font-semibold text-white">{comment.name}</p>
-                <p className="flex-none">
-                  <Checkbox />
-                </p>
+  id: number;
+  name: string;
+  imageUrl: string;
+  content: string;
+  date: string;
+  dateTime: string;
+};
+
+const comments: Comment[] = [
+  {
+    id: 1,
+    name: "Leslie Alexander",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    content:
+      "Explicabo nihil laborum. Saepe facilis consequuntur in eaque. Consequatur perspiciatis quam. Sed est illo quia. Culpa vitae placeat vitae. Repudiandae sunt exercitationem nihil nisi facilis placeat minima eveniet.",
+    date: "1d ago",
+    dateTime: "2023-03-04T15:54Z",
+  },
+  {
+    id: 2,
+    name: "Michael Foster",
+    imageUrl:
+      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    content:
+      "Laudantium quidem non et saepe vel sequi accusamus consequatur et. Saepe inventore veniam incidunt cumque et laborum nemo blanditiis rerum. A unde et molestiae autem ad. Architecto dolor ex accusantium maxime cumque laudantium itaque aut perferendis.",
+    date: "2d ago",
+    dateTime: "2023-03-03T14:02Z",
+  },
+  {
+    id: 3,
+    name: "Dries Vincent",
+    imageUrl:
+      "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    content:
+      "Quia animi harum in quis quidem sint. Ipsum dolorem molestias veritatis quis eveniet commodi assumenda temporibus. Dicta ut modi alias nisi. Veniam quia velit et ut. Id quas ducimus reprehenderit veniam fugit amet fugiat ipsum eius. Voluptas nobis earum in in vel corporis nisi.",
+    date: "2d ago",
+    dateTime: "2023-03-03T13:23Z",
+  },
+  {
+    id: 4,
+    name: "Lindsay Walton",
+    imageUrl:
+      "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    content:
+      "Unde dolore exercitationem nobis reprehenderit rerum corporis accusamus. Nemo suscipit temporibus quidem dolorum. Nobis optio quae atque blanditiis aspernatur doloribus sit accusamus. Sunt reiciendis ut corrupti ab debitis dolorem dolorem nam sit. Ducimus nisi qui earum aliquam. Est nam doloribus culpa illum.",
+    date: "3d ago",
+    dateTime: "2023-03-02T21:13Z",
+  },
+];
+
+export default function NewGoalsList(): JSX.Element {
+  return (
+    <ul role="list" className="">
+      {comments.map((comment) => (
+        <li key={comment.id} className=" gap-x-4 py-5">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
+              <CardTitle>{comment.name}</CardTitle>
+              <div className="flex items-center space-x-2">
+                <Checkbox id="terms" />
               </div>
-              <p className="mt-1 line-clamp-4 text-sm/6 text-gray-400">{comment.content}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-  
+            </CardHeader>
+            <CardContent className="pt-4">
+              <Label
+               
+                className="min-h-[120px] font-normal text-gray-500"
+               
+              >
+{comment.content}
+              </Label>
+            </CardContent>
+          </Card>
+        </li>
+      ))}
+    </ul>
+  );
+}
